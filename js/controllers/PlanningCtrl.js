815b0260282aa258947ab38397fb477bd53dfc4f
Nicolas Afresne <nicolas.afresne@gmail.com>
2014-05-19 08:38:39 +0200

    Clear test fullcalendar

--- js/controllers/PlanningCtrl.js
+++ js/controllers/PlanningCtrl.js
@@ -6,42 +6,10 @@ planningPHPTourApp.controller('planningCtrl', ['$scope','$http', function($scope
     $scope.viewTitle = {"session":"Sessions", "calendar":"Agenda", "split":"Split View"}
 
     //Vue courante
-    $scope.moduleState = 'calendar';
+    $scope.moduleState = 'session';
 
 	//Liste des filtres
 	$scope.filters = {"languages":"lang","salles":"salle"};
-
-    //Calendar
-    $scope.calendarConfig = new Object({
-        header: {
-            left: '',
-            center: '',
-            right: ''
-        },
-        year: 2014,
-        month: 5,
-        date: 23,
-        defaultView: "agendaWeek",
-        weekends:false,
-        hiddenDays: [ 3, 4, 5 ],
-        editable: false,
-        allDaySlot:false,
-        slotMinutes:15,
-        firstHour:9,
-        minTime:9,
-        maxTime:18,
-        h: 2500,
-        timeFormat: 'H(:mm)',
-        columnFormat: {
-            week: 'dddd dd MMMM'
-        },
-        axisFormat: 'HH:mm',
-        dayNames: ['Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche'],
-        dayNamesShort: ['Lun','Mar','Mer','Jeu','Thu','Ven','Sam','Dim'],
-        monthNames : ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
-    });
-
-    $scope.calendar = angular.element('#calendar').fullCalendar($scope.calendarConfig);
 
 	//Chargement des conférences
   	$http.get('data/data.json').success(function(data) {
